a1b3ad4d420ee8d876d68f18cde3541706a9680d
Zar Ni Htet <znh1684@gmail.com>
2023-09-14 02:52:20 -0700

    added default image to profile page

--- src/components/studentprofile/index.jsx
+++ src/components/studentprofile/index.jsx
@@ -8,6 +8,9 @@ const StudentProfile = () => {
     const [showStudent, setShowStudent] = useState();
     const [noData, setNoData] = useState(false);
 
+    const default_image = "https://images.unsplash.com/photo-1599834562135-b6fc90e642ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80";
+
+
     useEffect(() => {
         setTimeout(() => {
             const foundStudent = students.find((st) => st.id == studentId);
@@ -15,13 +18,12 @@ const StudentProfile = () => {
         }, 1000);
     }, [studentId, students])
 
-
     return (
         <div>
             Profile Page
             {showStudent ? (
                 <div>
-                    <img src={showStudent.img_url} alt={showStudent.student_data.name} />
+                    <img src={showStudent.img_url || default_image} alt={showStudent.student_data.name} />
                     <h1>{showStudent.student_data.name}</h1>
                     <p>{showStudent.student_data.age}</p>
                 </div>
